86a587705cac9d9dd228b6abaffd5ed04308729f
v324s <62812546+v324s@users.noreply.github.com>
2020-03-29 00:21:58 +0400

    Add files via upload

---
+++ Modeling agency/js/func.js
@@ -0,0 +1,121 @@
+info_glav=false;
+		info_brends=false;
+		info_team=false;
+		info_onas=false;
+		info_conts=false;
+		revers=false;
+		marginleft=0;
+		function slidr() {
+			if (goslide) {
+				if (revers==false && marginleft-marginleft-marginleft<($("#dor").innerWidth())*11/1.2) {
+					marginleft-=1;
+					$("#sliderbrand").css('margin-left',marginleft);
+				}else{
+					revers=true;
+					if (revers==true && marginleft<(($("#dor").innerWidth())*11)-($("#dor").innerWidth())*11/1.2) {
+						marginleft+=1;
+						$("#sliderbrand").css('margin-left',marginleft);
+					}else{
+						revers=false;
+					}
+				}
+			}
+		}
+		function slider() {
+			if (goslide){
+				setInterval(slidr,10);
+			}
+		}
+		
+		glav3_4=($("#slide_1").innerHeight())-($("#slide_1").innerHeight()/2);
+		glbrend=($("#slide_1").innerHeight())+($("#slide_2").innerHeight())-($("#slide_2").innerHeight()/2);
+		glbrona=($("#slide_1").innerHeight())+($("#slide_2").innerHeight())+($("#slide_3").innerHeight())-($("#slide_3").innerHeight()/2);
+		glbronkont=($("#slide_1").innerHeight())+($("#slide_2").innerHeight())+($("#slide_3").innerHeight())+($("#slide_4").innerHeight())-($("#slide_4").innerHeight()/1.5);
+		viewmap=false;
+		$('#openmap').click(function () {
+			if (!viewmap){
+				$('#framemap').show();
+				$('#map').show();
+				$('#openmap').text('закрыть карту');
+				viewmap=true;
+			}else{
+				$('#framemap').hide();
+				$('#map').hide();
+				$('#openmap').text('смотреть на карте');
+				viewmap=false;
+			}
+		});
+		$('#sliderbrand').click(function () {
+			if (goslide){
+				goslide=false;
+			}else{
+				goslide=true;
+			}
+		});
+		$(window).on("scroll", function() {
+			if ($(window).scrollTop() >0) {
+				$('#shapkamenu').css('background','#00000077');
+			}else{
+				$('#shapkamenu').css('background','none');
+			}
+			if (info_glav==false && $(window).scrollTop() >=0 && $(window).scrollTop() < glav3_4) {
+	    		info_glav=true;
+	    		info_brends=false;
+				info_team=false;
+				info_onas=false;
+				info_conts=false;
+	    		$('#navbtn_glav').css('border-bottom','2px solid');
+	    		$('#navbtn_brends').css('border-bottom','none');
+	    		$('#navbtn_team').css('border-bottom','none');
+	    		$('#navbtn_onas').css('border-bottom','none');
+	    		$('#navbtn_conts').css('border-bottom','none');
+	    	}
+	    	if (info_brends==false && $(window).scrollTop() > glav3_4) {
+	    		info_glav=false;
+	    		info_brends=true;
+				info_team=false;
+				info_onas=false;
+				info_conts=false;
+	    		$('#navbtn_glav').css('border-bottom','none');
+	    		$('#navbtn_brends').css('border-bottom','2px solid');
+	    		$('#navbtn_team').css('border-bottom','none');
+	    		$('#navbtn_onas').css('border-bottom','none');
+	    		$('#navbtn_conts').css('border-bottom','none');
+	    	}
+	    	if (info_team==false && $(window).scrollTop() > glbrend) {
+	    		info_glav=false;
+	    		info_brends=false;
+				info_team=true;
+				info_onas=false;
+				info_conts=false;
+	    		$('#navbtn_glav').css('border-bottom','none');
+	    		$('#navbtn_brends').css('border-bottom','none');
+	    		$('#navbtn_team').css('border-bottom','2px solid');
+	    		$('#navbtn_onas').css('border-bottom','none');
+	    		$('#navbtn_conts').css('border-bottom','none');
+	    	}
+	    	if (info_onas==false && $(window).scrollTop() > glbrona) {
+	    		info_glav=false;
+	    		info_brends=false;
+				info_team=false;
+				info_onas=true;
+				info_conts=false;
+	    		$('#navbtn_glav').css('border-bottom','none');
+	    		$('#navbtn_brends').css('border-bottom','none');
+	    		$('#navbtn_team').css('border-bottom','none');
+	    		$('#navbtn_onas').css('border-bottom','2px solid');
+	    		$('#navbtn_conts').css('border-bottom','none');
+	    	}
+	    	if (info_conts==false && $(window).scrollTop() > glbronkont) {
+	    		info_glav=false;
+	    		info_brends=false;
+				info_team=false;
+				info_onas=false;
+				info_conts=true;
+	    		$('#navbtn_glav').css('border-bottom','none');
+	    		$('#navbtn_brends').css('border-bottom','none');
+	    		$('#navbtn_team').css('border-bottom','none');
+	    		$('#navbtn_onas').css('border-bottom','none');
+	    		$('#navbtn_conts').css('border-bottom','2px solid');
+	    	}
+	    });
